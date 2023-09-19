f5a7cab9e2e74dc9b3ca359a1af7d90e3186dd7a
Repnikov.A <repnikov.a@florcom.ru>
2023-09-19 16:03:41 +0300

    На проверку

--- HW.13.6.2/Program.cs
+++ HW.13.6.2/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW._13._6._2
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        string[] words;
+        string path = @"C:\Users\repnikov.a\Desktop\Text_13_6_2.txt";
+
+        using (var sr = new StreamReader(path))
+        {
+            var text = sr.ReadToEnd().ToLower();
+
+            text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            words = text.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+        var result = words.GroupBy(x => x)
+            .Where(x => x.Count() > 1)
+            .Select(x => new { Word = x.Key, Frequency = x.Count() });
+
+
+        foreach (var item in result)
+        {
+            if (item.Frequency > 1)
+            {
+                dictionary.Add(item.Word, item.Frequency);
+            }
+        }
+
+
+        var orderedWords = dictionary.OrderByDescending(n => n.Value);
+        int i = 1;
+        foreach (var item in orderedWords)
+        {
+            Console.WriteLine($"{i}) Слово \"{item.Key}\" встречается {item.Value} раз");
+
+            if (i == 10)
+                break;
+            i++;
+        }
+    }
+}
+}
