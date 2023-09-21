07381961fa1aa4b8cdbe9681734671c48b23cd79
Repnikov.A <repnikov.a@florcom.ru>
2023-09-21 11:02:56 +0300

    Убрано условие вывода по счетчику. Использован Take()

--- HW.13.6.2/Program.cs
+++ HW.13.6.2/Program.cs
@@ -7,47 +7,45 @@ using System.IO;
 namespace HW._13._6._2
 {
     class Program
-{
-    static void Main(string[] args)
     {
-        Dictionary<string, int> dictionary = new Dictionary<string, int>();
-        string[] words;
-        string path = @"C:\Users\repnikov.a\Desktop\Text_13_6_2.txt";
-
-        using (var sr = new StreamReader(path))
+        static void Main(string[] args)
         {
-            var text = sr.ReadToEnd().ToLower();
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            string[] words;
+            string path = @"C:\Users\repnikov.a\Desktop\Text_13_6_2.txt";
 
-            text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
-
-            words = text.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-
-        var result = words.GroupBy(x => x)
-            .Where(x => x.Count() > 1)
-            .Select(x => new { Word = x.Key, Frequency = x.Count() });
-
-
-        foreach (var item in result)
-        {
-            if (item.Frequency > 1)
+            using (var sr = new StreamReader(path))
             {
-                dictionary.Add(item.Word, item.Frequency);
+                var text = sr.ReadToEnd().ToLower();
+
+                text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+                words = text.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
             }
-        }
 
 
-        var orderedWords = dictionary.OrderByDescending(n => n.Value);
-        int i = 1;
-        foreach (var item in orderedWords)
-        {
-            Console.WriteLine($"{i}) Слово \"{item.Key}\" встречается {item.Value} раз");
+            var result = words.GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .Select(x => new { Word = x.Key, Frequency = x.Count() });
 
-            if (i == 10)
-                break;
-            i++;
+
+            foreach (var item in result)
+            {
+                if (item.Frequency > 1)
+                {
+                    dictionary.Add(item.Word, item.Frequency);
+                }
+            }
+
+
+            var orderedWords = dictionary.OrderByDescending(n => n.Value).Take(10);
+            int i = 1;
+            foreach (var item in orderedWords)
+            {
+                Console.WriteLine($"{i}) Слово \"{item.Key}\" встречается {item.Value} раз");
+                i++;
+            }
+
         }
     }
-}
 }
